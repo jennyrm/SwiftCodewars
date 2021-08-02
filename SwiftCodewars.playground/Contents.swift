6ac50dd9758cfbfe359ef435c79fc8c594e5994e
Jenny Morales <50502203+jennyrm@users.noreply.github.com>
2021-08-02 10:28:36 -0700

    Digital Root 7kyu

--- SwiftCodewars.playground/Contents.swift
+++ SwiftCodewars.playground/Contents.swift
@@ -156,3 +156,31 @@ func find_short(_ str: String) -> Int {
 //find_short("bitcoin take over the world maybe who knows perhaps")
 //find_short("lets talk about javascript the best language")
 //find_short("i want to travel the world writing code one day")
+
+/**Digital root is the recursive sum of all the digits in a number.
+
+Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.*/
+
+//func digitalRoot(of number: Int) -> Int {
+//    let arrayOfDigits = String(number).compactMap { $0.wholeNumberValue }
+//
+//    let reducedNumber = arrayOfDigits.reduce(0, +)
+//
+//    if reducedNumber > 9 { return digitalRoot(of: reducedNumber) }
+//
+//    return reducedNumber
+//}
+
+func digitalRoot(of number: Int) -> Int {
+    let reducedNumber = String(number).compactMap { $0.wholeNumberValue }.reduce(0, +)
+
+    return reducedNumber > 9 ? digitalRoot(of: reducedNumber) : reducedNumber
+}
+ 
+//16  -->  1 + 6 = 7
+//942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+//132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+//493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+digitalRoot(of: 16)
+digitalRoot(of: 942)
