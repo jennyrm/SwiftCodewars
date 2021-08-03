dcc72ccadd0a8fa338c6c1a2fe8e9a5f1b811449
Jenny Morales <50502203+jennyrm@users.noreply.github.com>
2021-08-03 08:43:24 -0700

    Sum of Two Lowest Positive Numbers 7kyu

--- SwiftCodewars.playground/Contents.swift
+++ SwiftCodewars.playground/Contents.swift
@@ -184,3 +184,18 @@ func digitalRoot(of number: Int) -> Int {
 
 digitalRoot(of: 16)
 digitalRoot(of: 942)
+
+/**Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+ 
+ For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+ [10, 343445353, 3453445, 3453545353453] should return 3453455.*/
+
+func sumOfTwoSmallestIntegersIn(_ array: [Int]) -> Int {
+    let sortedArr = array.sorted()
+    
+    return sortedArr[0] + sortedArr[1]
+}
+
+sumOfTwoSmallestIntegersIn([19, 5, 42, 2, 77])
+sumOfTwoSmallestIntegersIn([10, 343445353, 3453445, 3453545353453])
