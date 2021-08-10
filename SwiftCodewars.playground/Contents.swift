a54755f8e59246185f5f6ae1d1d90bc0cc34b378
Jenny Morales <50502203+jennyrm@users.noreply.github.com>
2021-08-10 08:35:15 -0700

    Replace all items 7kyu

--- SwiftCodewars.playground/Contents.swift
+++ SwiftCodewars.playground/Contents.swift
@@ -259,5 +259,21 @@ func findSum(_ n: Int) -> Int {
     return sum
 }
 
-findSum(5)
-findSum(10)
+//findSum(5)
+//findSum(10)
+
+/**Write function replaceAll (Python: replace_all) that will replace all occurrences of an item with another.
+Python / JavaScript: The function has to work for strings and lists.
+Example: replaceAll [1,2,2] 1 2 -> in list [1,2,2] we replace 1 with 2 to get new list [2,2,2]*/
+
+func replaceAll<T: Equatable>(array: [T], old: T, new: T) -> [T] {
+    var newArr: [T] = []
+    
+    array.forEach {
+        $0 == old ? newArr.append(new) : newArr.append($0)
+    }
+    
+    return newArr
+}
+
+replaceAll(array: [1,2,2], old: 1, new: 2) // [2,2,2]
