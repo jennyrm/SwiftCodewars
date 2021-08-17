6e4eda63801b30a9051b6c0f5f752c44033a3dc7
Jenny Morales <50502203+jennyrm@users.noreply.github.com>
2021-08-17 09:16:33 -0700

    Odder Than the Rest 7kyu

--- SwiftCodewars.playground/Contents.swift
+++ SwiftCodewars.playground/Contents.swift
@@ -277,3 +277,18 @@ func replaceAll<T: Equatable>(array: [T], old: T, new: T) -> [T] {
 }
 
 //replaceAll(array: [1,2,2], old: 1, new: 2) // [2,2,2]
+
+/**
+ Create a function oddOne that takes an [Int] as input and outputs the index at which the sole odd number is located.
+ This method should work with arrays with negative numbers. If there are no odd numbers in the array, then the method should output nil.
+ Examples:
+ */
+
+func oddOne(_ arr: [Int]) -> Int? {
+    return arr.enumerated().filter({ $0.element % 2 != 0 }).map({ $0.offset }).first
+}
+
+//oddOne([2,4,6,7,10]) // => 3
+//oddOne([2,16,98,10,13,78]) // => 4
+//oddOne([4,-8,98,-12,-7,90,100]) // => 4
+//oddOne([2,4,6,8]) // => nil
