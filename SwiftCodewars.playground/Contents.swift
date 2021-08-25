2f326585e745ace236f9bde6711e059f7a5235e7
Jenny Morales <50502203+jennyrm@users.noreply.github.com>
2021-08-25 08:44:40 -0700

    Flatten and sort an array 7kyu

--- SwiftCodewars.playground/Contents.swift
+++ SwiftCodewars.playground/Contents.swift
@@ -342,6 +342,18 @@ func digits(num n: UInt64) -> Int {
     return String(n).count
 }
 
-digits(num: 5)
+digits(num: 0)
 digits(num: 12345)
 digits(num: 9876543210)
+
+/**
+ Given a two-dimensional array of integers, return the flattened version of the array with all the integers in the sorted (ascending) order.
+ Example:
+ Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
+ */
+
+func flattenAndSort<T: Comparable>(_ arr: [[T]]) -> [T] {
+    return arr.flatMap({ $0 }).sorted()
+}
+
+flattenAndSort([[3, 2, 1], [4, 6, 5], [], [9, 7, 8]])
