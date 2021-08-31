5735521eef0e0303f702d8d52535c02f861bd185
Jenny Morales <50502203+jennyrm@users.noreply.github.com>
2021-08-31 10:14:44 -0700

    Jaden Casing Strings 7kyu

--- SwiftCodewars.playground/Contents.swift
+++ SwiftCodewars.playground/Contents.swift
@@ -440,4 +440,24 @@ func longestSubstring(_ a: String, _ b: String) -> Int {
     return 0
 }
 
-longestSubstring("piquancy", "refocusing")
+//longestSubstring("piquancy", "refocusing")
+
+/**
+ Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
+
+ Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+
+ Example:
+
+ Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+ Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+ */
+
+extension String {
+    func toJadenCase() -> String {
+        return self.components(separatedBy: " ").map { return $0.first!.uppercased() + $0.dropFirst(1) }.joined(separator: " ")
+    }
+}
+
+let stringToTest = "most trees are blue"
+stringToTest.toJadenCase()
